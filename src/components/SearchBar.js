583e3f7ddf8eed0365d1150a0bdc5167fc2dbc8a
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-25 19:18:00 -0400

    Add SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SearchBar = () => {
+
+     return (
+          <input placeholder="Search"/>
+     )
+
+}
+
+export default SearchBar
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import useResults from "../hooks/useResults";
 import CountriesList from "../components/CountriesList"
+import SearchBar from "../components/SearchBar"
 
 const Homepage = () => {
+     const [term, setTerm] = useState("")
      
 const [searchCountries, results, error] = useResults()
+
+// const countriesFilter = country => {
+//      return results.filter(country => country.name.toLowerCase().includes(term.toLowerCase()))
+// }
+
      return (
           <>
-<h1>On the homepage</h1>
+<SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchCountries(term)}/>
 <CountriesList results={results}/>
 </>
      )
