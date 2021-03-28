88d45bc776fb0dbd82df14f32d4b1fa17bba442f
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-28 15:59:04 -0400

    Notes for later adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react"
 
 const Header = () => {
      return(
+
+          <>
           <h1>Where in the World?</h1>
+          <h6 style={{position: "absolute", top: "0", right: "0"}}>Darkmode/Lightmode button will go in header too</h6>
+          </>
      )
 
 }
